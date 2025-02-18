3eb5e93773fb8741e9bed79baf4392bdff99789a
Shrinath Mathane <shrinath.mathane@businessnext.com>
2025-02-18 13:02:04 +0530

    Report

--- src/main/java/com/qa/sound/listener/ExtentReportListener.java
+++ src/main/java/com/qa/sound/listener/ExtentReportListener.java
@@ -21,7 +21,7 @@ import java.util.Date;
 public class ExtentReportListener implements ITestListener {
 
     public static String filePath = "./reports/";
-    public static String fileName = System.currentTimeMillis()+"TestReport.html";
+    public static String fileName = "extent-report.html";
 
     public static WebDriver driver;
     public static ExtentReports extentReports = init();
